e21bb72537b9deb8fb33dafb815bea7c49dd0f2b
ogarkov43 <78510780+ogarkov43@users.noreply.github.com>
2026-01-18 01:16:45 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 // Конфигурация Vite для React приложения Weny.
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
+    plugins: [react()],
+    base: '/',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+    },
 });
