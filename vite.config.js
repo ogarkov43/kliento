c3cf1a7cea868d3f01f59cdf6e48e87d32a7a0d8
ogarkov43 <78510780+ogarkov43@users.noreply.github.com>
2026-01-18 00:34:52 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 // Конфигурация Vite для React приложения Weny.
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
 });
